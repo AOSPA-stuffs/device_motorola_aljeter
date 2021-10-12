ae8cc9ae5a1a499c8ad03f131ed471952871c6e8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 09:34:08 +0200

    msm8937-common: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: Ia8cef3905c133441cbe741c83d6d5ecdf4be2338

--- msm8937.mk
+++ msm8937.mk
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32
 
 PRODUCT_COPY_FILES += \
