eb51786cc5a15e2705492b4cf45b6c45ce5c5248
Alexander Koskovich <akoskovich@pm.me>
2022-09-14 09:16:39 +0200

    aljeter: Move Bluetooth power overlays to sysprops
     * These were moved to sysprops in Android 13.
    
       https://android.googlesource.com/platform/system/libsysprop/+/976d33062b51e77edde44faad1a6e801c5dc0188%5E%21/#F0
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I5bff67a15c537e8a47a19f30776fca6bf2c9fbe5 | AOSP
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- properties.mk
+++ properties.mk
@@ -41,7 +41,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=pronto \
-    bluetooth.device.default_name=Moto G6 Play
+    bluetooth.device.default_name=Moto G6 Play \
+    bluetooth.hardware.power.operating_voltage_mv=3300
 
 # Blur
 PRODUCT_PROPERTY_OVERRIDES += \
