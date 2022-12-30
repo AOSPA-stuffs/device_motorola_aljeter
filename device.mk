b4a48f0cfd4efe01f63341a398cf6cd43b985323
5H4ND <miqueiasantos20@gmail.com>
2022-12-30 20:42:25 -0300

    aljeter: Provide shim for libmmcamera_ppeiscore.so
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -362,6 +362,7 @@ PRODUCT_PACKAGES += \
 
 # Shims
 PRODUCT_PACKAGES += \
+    libui_shim \
     libcutils_shim \
     libmemset_shim \
     libqsapshim
--- extract-files.sh
+++ extract-files.sh
@@ -49,6 +49,10 @@ function blob_fixup() {
             "${PATCHELF}" --set-soname keystore.msm8937.so "${2}"
             ;;
 
+	vendor/lib/libmmcamera_ppeiscore.so)
+	    "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+	    ;;
+
         vendor/lib/libmot_gpu_mapper.so | vendor/lib/libmmcamera_vstab_module.so | vendor/lib/libjscore.so)
             sed -i "s/libgui/libwui/" "${2}"
             ;;
