7ad7461102769bb5e2ce9143c668c01c71fec722
SH4ND <miqueiasantos20@gmail.com>
2023-04-06 09:00:55 -0300

    aljeter: Update Bluetooth blobs from LA.UM.10.6.2.r1-02500-89xx.0
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -100,7 +100,9 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth@1.0.vendor
+    android.hardware.bluetooth@1.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Keymaster
 PRODUCT_PACKAGES += \
