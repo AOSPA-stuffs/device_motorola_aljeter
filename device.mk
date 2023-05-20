98dd1f8cd3660930f9be1458dd07fc69c5f154f4
SH4ND <miqueiasantos20@gmail.com>
2023-05-20 10:59:58 -0300

    Revert "msm8937-common: Update audio stack to R"
    This reverts commit f20b8ae410f6adc2353bc9046c21d4ae6b6b35bf.

--- device.mk
+++ device.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
