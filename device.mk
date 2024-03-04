6e7e89e2555e0e33088c7fd66dc1c0af17536730
Jprimero15 <jprimero15@aospa.co>
2024-03-04 14:17:19 -0300

    aljeter: Remove android.frameworks.sensorservice@1.0.vendor
    * AOSPA/android_vendor_aospa@82259df
    
    Change-Id: If780211eabf7554f5a3ca6d3b080dd59592a0d57
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -121,9 +121,6 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 # Sensors
-PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0.vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
